a21c4c6b40e38658f92824b55d66180f5897a465
einge23 <einge823@gmail.com>
2025-01-31 13:15:40 -0500

    update some things

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -8,16 +8,6 @@ import { update } from "./update";
 const require = createRequire(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.mjs   > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
 process.env.APP_ROOT = path.join(__dirname, "../..");
 
 export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
@@ -27,9 +17,6 @@ export const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL;
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
     ? path.join(process.env.APP_ROOT, "public")
     : RENDERER_DIST;
-
-// Disable GPU Acceleration for Windows 7
-if (os.release().startsWith("6.1")) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
 if (process.platform === "win32") app.setAppUserModelId(app.getName());
@@ -48,24 +35,15 @@ async function createWindow() {
         title: "Main window",
         width: 1040,
         height: 600,
+        frame: false,
         icon: path.join(process.env.VITE_PUBLIC, "favicon.ico"),
         webPreferences: {
             preload,
-
-            // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-            // nodeIntegration: true,
-
-            // Consider using contextBridge.exposeInMainWorld
-            // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-            // contextIsolation: false,
         },
     });
 
     if (VITE_DEV_SERVER_URL) {
-        // #298
         win.loadURL(VITE_DEV_SERVER_URL);
-        // Open devTool if the app is not packaged
-        win.webContents.openDevTools();
     } else {
         win.loadFile(indexHtml);
     }
